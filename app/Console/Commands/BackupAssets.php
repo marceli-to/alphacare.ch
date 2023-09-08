727dc497a568af6bd5bcc1652f8ad396d3a43cc7
Marcel Stadelmann <m@marceli.to>
2023-09-08 14:11:10 +0200

    wip

---
+++ app/Console/Commands/BackupAssets.php
@@ -0,0 +1,60 @@
+<?php
+namespace App\Console\Commands;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\File;
+
+class BackupAssets extends Command
+{
+  protected $signature = 'backup:assets';
+  
+  protected $description = 'Backup the assets folder';
+  
+  public function handle()
+  {
+    $source_path = base_path('public/assets');
+    $backup_path = storage_path('app/backups/' . date('Y-m-d'));
+
+    if (!File::exists($source_path))
+    {
+      $this->error('The content folder does not exist.');
+      return;
+    }
+
+    // Create the backup folder if it doesn't exist
+    if (!File::exists($backup_path))
+    {
+      File::makeDirectory($backup_path);
+    }
+
+    // Generate a unique backup filename
+    $backup_filename = date('Y-m-d_H-i') . '_assets.zip';
+    $backup_file = $backup_path . '/' . $backup_filename;
+
+    // Zip the content folder
+    $zip = new \ZipArchive();
+    if ($zip->open($backup_file, \ZipArchive::CREATE | \ZipArchive::OVERWRITE))
+    {
+      $files = new \RecursiveIteratorIterator(
+        new \RecursiveDirectoryIterator($source_path),
+        \RecursiveIteratorIterator::LEAVES_ONLY
+      );
+
+      foreach ($files as $name => $file)
+      {
+        if (!$file->isDir())
+        {
+          $filePath = $file->getRealPath();
+          $relativePath = substr($filePath, strlen($source_path) + 1);
+          $zip->addFile($filePath, $relativePath);
+        }
+      }
+      $zip->close();
+    } 
+    else
+    {
+      $this->error('Failed to create the backup zip file.');
+      return;
+    }
+    $this->info('Content folder has been successfully backed up to: ' . $backup_file);
+  }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -1,32 +1,31 @@
 <?php
-
 namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use App\Tasks\BackupJob;
 
 class Kernel extends ConsoleKernel
 {
-    /**
-     * Define the application's command schedule.
-     *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
-     * @return void
-     */
-    protected function schedule(Schedule $schedule)
-    {
-        // $schedule->command('inspire')->hourly();
-    }
+  /**
+   * Define the application's command schedule.
+   *
+   * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+   * @return void
+   */
+  protected function schedule(Schedule $schedule)
+  {
+    // $schedule->command('backup:content')->daily()->at('01:00');
+  }
 
-    /**
-     * Register the commands for the application.
-     *
-     * @return void
-     */
-    protected function commands()
-    {
-        $this->load(__DIR__.'/Commands');
-
-        require base_path('routes/console.php');
-    }
+  /**
+   * Register the commands for the application.
+   *
+   * @return void
+   */
+  protected function commands()
+  {
+    $this->load(__DIR__.'/Commands');
+    require base_path('routes/console.php');
+  }
 }
