30337ba24b7ec64782ee53ffb7973136091a97be
Marcel Stadelmann <m@marceli.to>
2023-10-02 15:50:43 +0200

    wip

--- resources/js/modules/team.js
+++ resources/js/modules/team.js
@@ -7,7 +7,7 @@
   };
 
   const selectors = {
-    btn: '[data-category-btn]',
+    select: '[data-categories]',
     btnReset: '[data-reset-btn]',
     items: '[data-category-item]',
   };
@@ -16,38 +16,26 @@
 
 
   const init = () => {
-    // on btn click get the data-btn-category attribute value
+    // on select change show items with the same data-item-category attribute value
     // and show the items with the same data-item-category attribute value
-    const btns = document.querySelectorAll(selectors.btn);
-    btns.forEach(btn => {
-      btn.addEventListener('click', () => {
-        const category = btn.getAttribute('data-category-btn');
-        activeCategory = category;
+    const select = document.querySelector(selectors.select);
 
-        // show reset btn
-        const resetBtn = document.querySelector(selectors.btnReset);
-        resetBtn.classList.remove(classes.hidden);
+    select.addEventListener('change', () => {
+      const category = select.value;
+      activeCategory = category;
 
-        // add class active to the clicked btn
-        btns.forEach(btn => {
-          btn.classList.remove(classes.active);
-        });
-        btn.classList.add(classes.active);
+      // show reset btn
+      const resetBtn = document.querySelector(selectors.btnReset);
+      resetBtn.classList.remove(classes.hidden);
 
-        // hide all items
-        const itemsToHide = document.querySelectorAll(selectors.items);
-        itemsToHide.forEach(item => {
-          item.classList.remove(classes.visible);
-          item.classList.add(classes.hidden);
-        });
+      hideAll();
 
-        // show selected item with the same data-category-item attribute value
-        const itemsToShow = document.querySelectorAll(`[data-category-item*="${category}"]`);
-        itemsToShow.forEach(item => {
-          item.classList.remove(classes.hidden);
-          item.classList.add(classes.visible);
-        });
-      });
+      if (category === 'all') {
+        showAll();
+        return;
+      }
+
+      showSelected(category);
     });
 
     // on reset btn click show all items
@@ -56,17 +44,35 @@
       // hide reset btn
       resetBtn.classList.add(classes.hidden);
 
-      // remove active class from all btns
-      btns.forEach(btn => {
-        btn.classList.remove(classes.active);
-      });
+      showAll();
 
-      // show all items
-      const itemsToShow = document.querySelectorAll(selectors.items);
-      itemsToShow.forEach(item => {
-        item.classList.remove(classes.hidden);
-        item.classList.add(classes.visible);
-      });
+      // select first option
+      const select = document.querySelector(selectors.select);
+      select.value = 'all';
+    });
+  };
+
+  const showAll = () => {
+    const itemsToShow = document.querySelectorAll(selectors.items);
+    itemsToShow.forEach(item => {
+      item.classList.remove(classes.hidden);
+      item.classList.add(classes.visible);
+    });
+  };
+
+  const hideAll = () => {
+    const itemsToHide = document.querySelectorAll(selectors.items);
+    itemsToHide.forEach(item => {
+      item.classList.remove(classes.visible);
+      item.classList.add(classes.hidden);
+    });
+  };
+
+  const showSelected = (category) => {
+    const itemsToShow = document.querySelectorAll(`[data-category-item*="${category}"]`);
+    itemsToShow.forEach(item => {
+      item.classList.remove(classes.hidden);
+      item.classList.add(classes.visible);
     });
   };
 
